421447e15ba6e732960deed984514ea7393caf44
Braeden Wright <braeden.wright21@gmail.com>
2022-01-06 15:46:43 -0600

    main

---
+++ tables_exercises.sql
@@ -0,0 +1,19 @@
+
+
+
+SHOW TABLES;
+USE employees;
+
+#employees table uses  int, varchar, date
+
+#dept_emp, dept_manager, employees, salaries, and titles all contain numeric column types
+
+#departments, dept_emp, dept_manager, employees, and titles all contain string column types
+
+#dept_emp, dept_manager, employees, salaries, and titles all contain date column types
+
+#While there is no direct relationship between the employees and departments tables, the dept_emp table would directly correlate off these two tables#
+
+
+
+SHOW CREATE TABLE dept_manager;
