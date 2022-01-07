b749ca0bef0df218e8a1c1489d4ce5c319889764
Braeden Wright <braeden.wright21@gmail.com>
2022-01-07 15:03:47 -0600

    main

---
+++ functions_exercises.sql
@@ -0,0 +1,62 @@
+-- copy of ORDER BY text
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+#2.  Irena Reutenauer/ Vidya Simmen
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+#3. Irena Acton/ Vidya Zweizig
+
+SELECT last_name, first_name 
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+#4.   Acton, Irena/ Zyda, Maya
+
+SELECT DISTINCT *
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%e'
+ORDER BY emp_no;
+#5.  899 employees, 10021 Ramzi Erde/ 499648 Tadahiro Erde
+
+SELECT DISTINCT *
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%e'
+ORDER BY hire_date DESC;
+#6.   899 employees, Teiji Eldridge/ Sergi Erde
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY birth_date, hire_date DESC;
+#7.   362 employees, Khun Bernini, Douadi Pettis
+
+-- start of functions exercises
+
+SELECT CONCAT(first_name, ' ',last_name) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%e';
+
+
+SELECT Upper(CONCAT(first_name, ' ', last_name)) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%e';
+
+
+SELECT datediff(hire_date, curdate()) AS days_with_company
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY days_with_company;
+
+
